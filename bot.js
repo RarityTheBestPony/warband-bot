fd022247e5c76c19ec8f5387255a20787af56f30
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2018-12-25 14:55:33 +0500

    Replaced the main code

--- bot.js
+++ bot.js
@@ -1,46 +1,29 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-/*logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
-logger.level = 'debug';*/
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
-/*bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});*/
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    switch(message) {
-        case 'Шинку':
-        case 'шинку':
-            bot.sendMessage({
-                to: channelID,
-                message: 'Шинку пидор!'
-            });
-            break;
-        case 'Ашот':
-        case 'ашот':
-            bot.sendMessage({
-                to: channelID,
-                message: 'Ашот пидор!'
-            });
-            break;
-        case 'Рарити':
-        case 'рарити':
-            bot.sendMessage({
-                to: channelID,
-                message: 'Рарити молодец!'
-            });
-            break;
-     }
-});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.NTI3MDUxOTQ1OTI4NTU2NTU0.DwOHSA.HQZromI6nfUwT7ljjTN20fxY7l8);
