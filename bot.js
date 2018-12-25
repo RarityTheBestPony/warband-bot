06f54efb2f50d3d36be1e7e59901ed01d51bced1
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2018-12-25 15:04:32 +0500

    BOT_TOKEN retrieved

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NTI3MDUxOTQ1OTI4NTU2NTU0.DwOHSA.HQZromI6nfUwT7ljjTN20fxY7l8);
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
