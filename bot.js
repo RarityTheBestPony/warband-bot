a97a70e17e47b7b5bfb71b2337fb3017cf85b2c0
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-01-06 23:22:31 +0500

    Whatever

--- bot.js
+++ bot.js
@@ -1,43 +1,22 @@
 const Discord = require('discord.js');
-const net = require('net');
-const bChannelID = '501417274016661504';
+
+const bot = new Discord.Client();
 
  
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
+
+bot.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
  
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    switch(true) {
-        case (message == '#server'):
-            var client = new net.Socket();
 
-            client.connect(6553, '178.63.11.244', function() {
-                console.log('Connected');
-            });
-            client.on('data', function(data) {
-                var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
-                if(numberOfPlayers > 0){
-                    bot.sendMessage({
-                        to: bChannelID,
-                        message: 'На сервере ' + numberOfPlayers + " игроков! Заходите играть!"
-                    });
-                } else{
-                    bot.sendMessage({
-                        to: bChannelID,
-                        message: 'На сервере никого нет! Может пора начать?'
-                    });
-                }
-                
-                client.destroy(); // kill client after server's response
-            });
-            break;
-        case (message.toLowerCase().indexOf("шинку") == -1):
+    switch(message) {
+        case 'Шинку':
+        case 'шинку':
             bot.sendMessage({
                 to: channelID,
                 message: 'Шинку пидор!'
@@ -109,5 +88,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 message: 'Фенрир, че делал на выходных?'
             });
             break;
+        
      }
+
 });
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+bot.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
