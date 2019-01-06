287ddad4f09e032de415074e252dbc436d413b85
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-01-06 23:03:14 +0500

    Putting token inside the file

--- bot.js
+++ bot.js
@@ -123,5 +123,4 @@ client.on('message', function (user, userID, channelID, message, evt) {
  
 
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTI3MDUxOTQ1OTI4NTU2NTU0.DwOHSA.HQZromI6nfUwT7ljjTN20fxY7l8);
