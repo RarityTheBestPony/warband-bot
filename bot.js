724eb7bd38398d1c3b0baa32707b51adfa3fa974
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-01-06 23:58:23 +0500

    Okay it's done

--- bot.js
+++ bot.js
@@ -19,24 +19,26 @@ bot.on('ready', () => {
 bot.on('message', message => {
 
     if (message.content === '#server') {
-       var client = new net.Socket();
+        var client = new net.Socket();
 
-       client.connect(6553, '178.63.11.244', function() {
-           console.log('Connected');
-       });
-     
-       client.on('data', function(data) {
-           var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
-           if(numberOfPlayers > 0){
-               message.reply('На сервере ' + numberOfPlayers + " игроков! Заходите играть!");
-           } else{
-               message.reply('На сервере никого нет! Может пора начать?');
-           }
-           client.destroy(); // kill client after server's response
-       });
-       
+        client.connect(6553, '178.63.11.244', function() {
+            console.log('Connected');
+        });
 
-       }
+        client.on('data', function(data) {
+            var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
+            if(numberOfPlayers > 1){
+                message.reply('На сервере ' + numberOfPlayers + " игроков! Заходите играть!");
+            }
+            if(numberOfPlayers == 0){
+                message.reply('На сервере никого нет! Может пора начать?');
+            }
+            if(numberOfPlayers == 1){
+                message.reply('На сервере всего один игрок! Ему наверняка одиноко, заходи и поиграй с ним!');
+            } 
+            client.destroy(); // kill client after server's response
+        });
+    }
 
 });
 
