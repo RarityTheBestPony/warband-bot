4b6be3ac43cd7923a0c717891eb70a81ab0f1bc6
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-01-06 23:29:27 +0500

    Okay another try

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
+var net = require('net');
+
+const bChannelID = '501417274016661504';
+
  
 
 bot.on('ready', () => {
@@ -14,11 +18,112 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 
-    if (message.content === 'ping') {
+    switch(message) {
 
-       message.reply('pong');
+        case '#server':
+        case 'есть кто на сервере?':
+        case 'Есть кто на сервере?':
+        case 'играете?':
+        case 'Играете?':
+        case 'есть кто?':
+        case 'Есть кто?':
+            var client = new net.Socket();
 
-       }
+            client.connect(6553, '178.63.11.244', function() {
+                console.log('Connected');
+            });
+            client.on('data', function(data) {
+                var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
+                if(numberOfPlayers > 0){
+                    bot.sendMessage({
+                        to: bChannelID,
+                        message: 'На сервере ' + numberOfPlayers + " игроков! Заходите играть!"
+                    });
+                } else{
+                    bot.sendMessage({
+                        to: bChannelID,
+                        message: 'На сервере никого нет! Может пора начать?'
+                    });
+                }
+                
+                client.destroy(); // kill client after server's response
+            });
+            break;
+        case 'Шинку':
+        case 'шинку':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Шинку пидор!'
+            });
+            break;
+        case 'Сырный':
+        case 'сырный':
+        case 'Сырно':
+        case 'сырно':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Сырный пидор!'
+            });
+            break;
+        case 'Ашот':
+        case 'ашот':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Ашот пидор!'
+            });
+            break;
+        case 'Ингнар':
+        case 'ингнар':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Ингнар молодец!'
+            });
+            break;
+        case 'Рарити':
+        case 'рарити':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Рарити молодец!'
+            });
+            break;
+        case 'Мариса':
+        case 'мариса':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Мариса трап!'
+            });
+            break;
+        case 'Данила':
+        case 'данила':
+        case 'дани':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Данила любит Рикардо!'
+            });
+            break;
+        case 'Хельвете':
+        case 'хельвете':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Хельвете даун!'
+            });
+            break;
+        case 'Кулсон':
+        case 'кулсон':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Кулсон поставь сервак!'
+            });
+            break;
+        case 'Фенрир':
+        case 'фенрир':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Фенрир, че делал на выходных?'
+            });
+            break;
+        
+     }
 
 });
 
