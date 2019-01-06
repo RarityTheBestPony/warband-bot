7b726cf2c3e06156f093f6daa00cb8266cd733f4
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-01-06 22:50:21 +0500

    Bot can now connect to server

--- bot.js
+++ bot.js
@@ -1,55 +1,120 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-const client = new Discord.Client();
-
- 
-
-client.on('ready', () => {
+bot.on('ready', () => {
 
     console.log('I am ready!');
 
 });
 
- 
-
-client.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function (user, userID, channelID, message, evt) {
 
     switch(message) {
 
+        case '#server':
+        case 'есть кто на сервере?':
+        case 'Есть кто на сервере?':
+        case 'играете?':
+        case 'Играете?':
+        case 'есть кто?':
+        case 'Есть кто?':
+            var client = new net.Socket();
+
+            client.connect(6553, '178.63.11.244', function() {
+                console.log('Connected');
+            });
+            client.on('data', function(data) {
+                var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
+                if(numberOfPlayers > 0){
+                    bot.sendMessage({
+                        to: bChannelID,
+                        message: 'На сервере ' + numberOfPlayers + " игроков! Заходите играть!"
+                    });
+                } else{
+                    bot.sendMessage({
+                        to: bChannelID,
+                        message: 'На сервере никого нет! Может пора начать?'
+                    });
+                }
+                
+                client.destroy(); // kill client after server's response
+            });
+            break;
         case 'Шинку':
         case 'шинку':
             bot.sendMessage({
                 to: channelID,
                 message: 'Шинку пидор!'
             });
-        break;
+            break;
+        case 'Сырный':
+        case 'сырный':
+        case 'Сырно':
+        case 'сырно':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Сырный пидор!'
+            });
+            break;
         case 'Ашот':
         case 'ашот':
             bot.sendMessage({
                 to: channelID,
                 message: 'Ашот пидор!'
             });
-        break;
+            break;
         case 'Ингнар':
         case 'ингнар':
             bot.sendMessage({
                 to: channelID,
-                message: 'Ингнар красавчик!'
+                message: 'Ингнар молодец!'
             });
-        break;
+            break;
         case 'Рарити':
         case 'рарити':
             bot.sendMessage({
                 to: channelID,
-                message: 'Рарити красавчик!'
+                message: 'Рарити молодец!'
             });
-        break;
+            break;
+        case 'Мариса':
+        case 'мариса':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Мариса трап!'
+            });
+            break;
+        case 'Данила':
+        case 'данила':
+        case 'дани':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Данила любит Рикардо!'
+            });
+            break;
+        case 'Хельвете':
+        case 'хельвете':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Хельвете даун!'
+            });
+            break;
+        case 'Кулсон':
+        case 'кулсон':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Кулсон поставь сервак!'
+            });
+            break;
+        case 'Фенрир':
+        case 'фенрир':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Фенрир, че делал на выходных?'
+            });
+            break;
         
      }
 });
-
  
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NTI3MDUxOTQ1OTI4NTU2NTU0.DwOHSA.HQZromI6nfUwT7ljjTN20fxY7l8);
+bot.login(process.env.BOT_TOKEN);
