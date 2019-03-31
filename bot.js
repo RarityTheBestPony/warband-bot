45fcbb195b7ab2d16e4113d8cfb8f945925946f8
RarityTheBestPony <46129498+RarityTheBestPony@users.noreply.github.com>
2019-03-31 15:52:48 +0500

    Updated dead server message

--- bot.js
+++ bot.js
@@ -20,22 +20,22 @@ bot.on('message', message => {
 
     if (message.content === '!server') {
         var client = new net.Socket();
+        message.reply('Сервер умер. Помянем.');
+        //client.connect(6553, '178.63.11.244', function() {
+        //    console.log('Connected');
+        //});
 
-        client.connect(6553, '178.63.11.244', function() {
-            console.log('Connected');
-        });
-
-        client.on('data', function(data) {
-            var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
-            if(numberOfPlayers > 1){
-                message.reply('На сервере ' + numberOfPlayers + " игроков! Заходите играть!");
-            }
-            if(numberOfPlayers == 0){
-                message.reply('На сервере никого нет! Может пора начать?');
-            }
-            if(numberOfPlayers == 1){
-                message.reply('На сервере всего один игрок! Ему наверняка одиноко, заходи и поиграй с ним!');
-            } 
+        //client.on('data', function(data) {
+        //    var numberOfPlayers = parseInt(data.toString().split('<NumberOfActivePlayers>').pop().split('</NumberOfActivePlayers>')[0]);
+        //    if(numberOfPlayers > 1){
+        //        message.reply('На сервере ' + numberOfPlayers + " игроков! Заходите играть!");
+        //    }
+        //    if(numberOfPlayers == 0){
+        //        message.reply('На сервере никого нет! Может пора начать?');
+        //    }
+        //    if(numberOfPlayers == 1){
+        //        message.reply('На сервере всего один игрок! Ему наверняка одиноко, заходи и поиграй с ним!');
+        //    } 
             client.destroy(); // kill client after server's response
         });
     }
